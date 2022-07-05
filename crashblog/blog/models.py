f3b940d8c7ac2356659cf9a35837aa11627ab672
kolapaneni <balukolapaneni137@gmail.com>
2022-07-05 13:23:19 +0530

    second_init

--- crashblog/blog/models.py
+++ crashblog/blog/models.py
@@ -35,7 +35,7 @@ class Post(models.Model):
         ordering = ('-created_at',)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
 
     def get_absolute_url(self):
         return '/%s/%s' % (self.category.slug,self.slug)
